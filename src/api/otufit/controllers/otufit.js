88de92ddfe939fbd70e6babc94f0b21ebfc45357
ragonedaniele <ragonedaniele@gmail.com>
2022-12-14 19:46:15 +0100

    Initial commit

---
+++ src/api/otufit/controllers/otufit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * otufit controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::otufit.otufit');
---
+++ src/api/otufit/services/otufit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * otufit service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::otufit.otufit');
